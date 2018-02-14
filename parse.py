3024e9f2c14c119dbe6e0d66493bb7ff53264f28
Donia Tung <doniatung32@gmail.com>
2018-02-14 13:19:24 -0500

    lolz

--- parse.py
+++ parse.py
@@ -5,9 +5,16 @@ All restaurants in a specified zip code and with a specified grade.
 All restaurants in a specified zip code with a score below a specified threshold.
 Something more clever.
 '''
-c=MongoClient("lisa.stuy.edu",27017);
+
+from pymongo import MongoClient
+
+c=MongoClient("localhost",27017);
 mfDB=c['test']
 collie=mfDB['restaurants']
 def find_boroughs(borough):
-    for each in collie.find({"borough":borough}):
-        print each["name"]
+   dictionary = collie.find({"borough":borough})
+   for each in dictionary:
+       print each
+       
+
+find_boroughs("Manhattan")
