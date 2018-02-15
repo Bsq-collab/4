d117f9f66f52d11f9ae774b3973ee9cb12ea7b30
Bayan Berri <bberri@stuy.edu>
2018-02-14 20:02:59 -0500

    should work but not tested

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 '''
-All restaurants in a specified borough.
-All restaurants in a specified zip code.
+All restaurants in a specified borough. DONE
+All restaurants in a specified zip code. DONE
 All restaurants in a specified zip code and with a specified grade.
 All restaurants in a specified zip code with a score below a specified threshold.
 Something more clever.
@@ -8,13 +8,41 @@ Something more clever.
 
 from pymongo import MongoClient
 
-c=MongoClient("localhost",27017);
-mfDB=c['test']
-collie=mfDB['restaurants']
+c=MongoClient("lisa.stuy.edu",27017);
+collie=c.test.restaurants
+
+#mfDB=c['test']
+#collie=mfDB['restaurants']
 def find_boroughs(borough):
    dictionary = collie.find({"borough":borough})
    for each in dictionary:
-       print each
-       
+       print each["name"]
+   return dictionary
 
-find_boroughs("Manhattan")
+def by_zip(code):
+    dictionary_zip=collie.find({"zipcode":code})
+    for each in dictionary_zip:
+        print each["name"]
+    return dictionary_zip
+
+def by_zip_grade(zc,grade):
+    dictionary=collie.find({"$and":[{"zipcode":zc},{"grade":grade}]})
+    for each in dictionary:
+        print each["name"]
+    return dictionary
+
+def by_zip_score(zc,threshold):
+    dictionary=collie.find({"$and":[{"zipcode":zc},{"score":{"$lt":threshold}}]})
+    for each in dictionary:
+        print each["name"]
+    return dictionary
+
+for each in collie.find():
+    print each
+
+print "=====================BOROUGH==================\n"
+#find_boroughs("Manhattan")
+print "=====================ZIP==================\n"
+by_zip('11218')
+print "=====================ZG==================\n"
+#by_zip_grade(11220,"A")
